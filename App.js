b5dda18d4bba614a586fe6ebd94584f5129fa767
shruti0896 <ashna.shruti96@gmail.com>
2023-09-04 12:11:38 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+[React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", { id: "heading" }, "I am H1"),
+    React.createElement("h2", { id: "heading" }, "I am H2")]
+  ),
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", { id: "heading" }, "I am H1"),
+    React.createElement("h2", { id: "heading" }, "I am H2")]
+  )]
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
