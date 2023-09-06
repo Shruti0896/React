6dba24952dcc32cda2150054e957abc059ac518e
Shrut Singh <ashna.shruti96@gmail.com>
2023-09-06 21:05:42 +0530

    Third commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement(
   "div",
   { id: "parent" },
 [React.createElement(
     "div",
     { id: "child" },
-    [React.createElement("h1", { id: "heading" }, "I am H1"),
+    [React.createElement("h1", { id: "heading" }, "I am Sruti"),
     React.createElement("h2", { id: "heading" }, "I am H2")]
   ),
   React.createElement(
